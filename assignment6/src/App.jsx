a579497997b8b4b3c55664769cada089089a35de
zahidhossin39 <zahidhossin39@gmail.com>
2026-04-05 19:56:42 +0600

    assignment-6 started

---
+++ assignment6/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NavBar from './component/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ assignment6/src/component/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            yo
+        </div>
+    );
+};
+
+export default NavBar;
